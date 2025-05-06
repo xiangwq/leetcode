bb12de423c0d76c29c8beb245ad01420261ec4ab
xiangwenqi <2278031498@qq.com>
2025-05-06 21:24:37 +0800

    top100 - 226

--- top100/226.go
+++ top100/226.go
@@ -4,11 +4,10 @@ package top100
 // 输入：root = [4,2,7,1,3,6,9]
 // 输出：[4,7,2,9,6,3,1]
 func invertTree(root *TreeNode) *TreeNode {
-	if root == nil {
-		return root
+	if root != nil {
+		root.Left, root.Right = root.Right, root.Left
+		invertTree(root.Left)
+		invertTree(root.Right)
 	}
-	root.Left, root.Right = root.Right, root.Left
-	invertTree(root.Left)
-	invertTree(root.Right)
 	return root
 }
